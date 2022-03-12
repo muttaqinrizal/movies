6516de13e3a801b4a17fd3addbd17254792bbbe7
muttaqinrizal <rizalm57@gmail.com>
2022-03-12 20:03:21 +0700

    [REMOVE] unused import

--- src/helper/loading.js
+++ src/helper/loading.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Skeleton from "@mui/material/Skeleton";
 
 export default function Loading() {
   return (
--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,4 +1,4 @@
-import { gql, useLazyQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
 import ErrorMessage from "../../helper/errorMessage";
 import Loading from "../../helper/loading";
